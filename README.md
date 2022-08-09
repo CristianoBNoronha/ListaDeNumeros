9025c1e6a8dc5d2efa49e6f28cce367fcc9c4980
CristianoBNoronha <cris_nor@hotmail.com>
2022-08-09 11:11:34 -0300

    Add lista_numero.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ListaDeNumeros
 Exercise following Python course specifications, to list numbers.
+
+<a>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" width="60" height="60"/>
+<a/>
---
+++ lista_numero.py
@@ -0,0 +1,27 @@
+
+lnum = []
+n = 1
+while n > 0:
+    n = int(input("digite o número"))
+    if n > 0:
+        lnum.append(n)
+
+tam = len(lnum) - 1
+while tam >= 0:
+    print(lnum[tam], end=", ")
+    tam = tam - 1
+
+    
+        
+        
+            
+
+
+    
+
+
+
+
+
+        
+        
